e573f82f5872a4ed851359dbe32b166a308198dc
steve <sbutler@soldermonkey.com>
2016-11-19 23:14:01 +0000

    Removed s from "Hundreds" Label

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ string translateNumber(int num){
 			     "Eighteen","Nineteen"};
 	const string tensLabels[] = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
 
-	if (num >= 100)	return translateNumber(num / 100).append(" Hundreds ").append(translateNumber(num % 100));
+	if (num >= 100)	return translateNumber(num / 100).append(" Hundred ").append(translateNumber(num % 100));
 	if (num >= 20) return string().append(tensLabels[(num/10)-2]).append(" ").append(translateNumber(num % 10));
 	return onesLabels[num];
 }
