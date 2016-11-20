84d51433a4e6acce751ff16451ef9e39048355d5
Steve Butler <sbutler@soldermonkey.com>
2016-11-19 20:01:52 -0500

    Added non-recursive version

---
+++ noRecursion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string translateNumber(int num){
+	const string onesLabels[] = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine",
+			     "Ten","Eleven","Twelve","Thirteen","Fourteen","Fiveteen","Sixteen","Seventeen"
+			     "Eighteen","Nineteen"};
+	const string tensLabels[] = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+
+	string result = "";
+
+	if (num >= 100)	{
+		result.append(onesLabels[num / 100]).append(" Hundreds ");
+		num %= 100;
+	}
+	if (num >= 20) {
+		result.append(tensLabels[(num/10)-2]).append(" ");
+		num %= 10;
+	}
+	if (num > 0){
+		result.append(onesLabels[num]);
+	}
+
+	return result;
+}
+
+int main(){
+	cout << translateNumber(913) << endl;
+}
